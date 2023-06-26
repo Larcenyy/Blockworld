164a084bbd33b25da7ef6311db6308bbe0f5a507
Remy <remyc59440@hotmail.com>
2023-06-26 11:54:18 +0200

    26/06/23 - v1.2

--- views/home.blade.php
+++ views/home.blade.php
@@ -7,35 +7,36 @@
          style="background: linear-gradient(0deg, rgba(51,54,41,1) 10%, rgba(0,219,255,0) 100%)  , url('{{ setting('background') ? image_url(setting('background')) : 'https://via.placeholder.com/2000x500' }}') center / cover no-repeat; height: 1000px">
         <h3>{{ trans('messages.welcome', ['name' => site_name()]) }}</h3>
         <p class="w-50 text-xl-center">{{theme_config("header.hero.description")}}</p>
+
         <div class="mt-3">
             <a href="" class="btnJoin text-myBackground" data-aos="fade-down">{{theme_config("header.hero.btn")}}</a>
         </div>
 
         @if(theme_config('header.box.toggle'))
-        <div class="d-flex flex-row gap-1 w-100 justify-content-around">
-            <div class="d-flex rounded-1 gap-3 align-items-center cursorAnim d-flex  justify-content-center text-myBackground p-3 borderType rounded-1 w-25">
-                <div class="d-flex flex-column align-items-center">
-                    <p class="d-flex  align-items-center gap-3">
-                        <i class="bi bi-person-fill fs-3"></i>
-                        <em class="fs-6">33 inscrits sur notre site</em>
-                    </p>
-                    <p class="fs-4 firstLetter">
-                        Membres Inscrits
-                    </p>
+            <div class="d-flex flex-row gap-1 w-100 justify-content-around">
+                <div class="d-flex rounded-1 gap-3 align-items-center cursorAnim d-flex  justify-content-center text-myBackground p-3 borderType rounded-1 w-25">
+                    <div class="d-flex flex-column align-items-center">
+                        <p>
+                            <i class="{{theme_config("header.box.left.iconBoxLeft")}}"></i>
+                            <em class="fs-6">{{theme_config("header.box.left.titleBoxLeft")}}</em>
+                        </p>
+                        <p class="fs-4 firstLetter">
+                            {{theme_config("header.box.left.descripBoxLeft")}}
+                        </p>
+                    </div>
+                </div>
+                <div class="d-flex rounded-1 gap-3 align-items-center cursorAnim d-flex  justify-content-center text-myBackground p-3 borderType rounded-1 w-25">
+                    <div class="d-flex flex-column align-items-center">
+                        <p>
+                            <i class="{{theme_config("header.box.right.iconBoxRight")}}"></i>
+                            <em class="fs-6">{{theme_config("header.box.right.titleBoxRight")}}</em>
+                        </p>
+                        <p class="fs-4 firstLetter">
+                            {{theme_config("header.box.right.descripBoxRight")}}
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div class="d-flex rounded-1 gap-3 align-items-center cursorAnim d-flex  justify-content-center text-myBackground p-3 borderType rounded-1 w-25">
-                <div class="d-flex flex-column align-items-center">
-                    <p class="d-flex  align-items-center gap-3">
-                        <i class="bi bi-person-fill fs-3"></i>
-                        <em class="fs-6">33 inscrits sur notre site</em>
-                    </p>
-                    <p class="fs-4 firstLetter">
-                        Membres Inscrits
-                    </p>
-                </div>
-            </div>
-        </div>
         @endif
 
         @if($server)
@@ -221,5 +222,5 @@
 
 
 <script>
-    let videourl = "{{theme_config("video.link.url")}}";
+    let videourl = "https://www.youtube.com/embed/{{theme_config("video.link.url")}}";
 </script>
